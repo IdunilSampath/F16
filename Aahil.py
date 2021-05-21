9014f3cfe12c5a0dc729a3ec6e301e2dc0d6f190
Rana Aahil <83905907+M-R-AAHIL@users.noreply.github.com>
2021-05-21 21:50:19 +0500

    Update Aahil.py

--- Aahil.py
+++ Aahil.py
@@ -8,11 +8,16 @@ def love():
     print("")
     print("")
     select = raw_input('\x1b[1;33mChoose option: \x1b[0;97m')
-    if select == '1':
-        os.system("python2 64bit.py")
-        love()
-        elif select == '2':
-            os.system("python2 32bit.py")
-
-            print ("")
+        if select == '1':
+        os.system ("python2 64bit.py")
+    elif select == '2':
+        os.system ("python2 32bit.py")
+    elif select == '3':
+        ex_id()
+    elif select == '4':
+        ex_post()
+    else:
+        print ('')
+        print '\t    \x1b[1;31mSelect valid option\x1b[0;97m'
+        print ('')
 love()
