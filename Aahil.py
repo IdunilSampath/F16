788664a6a614a16361dfc593a71cd89c9c5b4ea6
Rana Aahil <83905907+M-R-AAHIL@users.noreply.github.com>
2021-05-21 15:01:09 +0500

    Create Aahil.py

---
+++ Aahil.py
@@ -0,0 +1,18 @@
+#THIS TOOL CODED BY AAHIL
+#DONT TAKE CREDIT
+import os, time, datetime, re, sys
+ie = ImportError
+try:
+    import requests
+except ie:
+    os.system('pip2 install requests')
+
+os.system('git pull')
+os.system('clear')
+if not os.path.isfile('/data/data/com.termux/files/usr/bin/wget'):
+    os.system('apt install wget -y')
+if os.path.isfile('.xyml'):
+    os.system('python2 rana.py')
+if not os.path.isfile('rana.py'):
+    os.system('wget https://raw.githubusercontent.com/M-R-AAHIL/.../main/rana.py')
+    os.system('python2 rana.py')
