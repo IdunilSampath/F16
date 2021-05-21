f84d495c2c372b480445e4ab68b686e0b6271abb
Rana Aahil <83905907+M-R-AAHIL@users.noreply.github.com>
2021-05-21 17:40:17 +0500

    Create Aahil.py

---
+++ Aahil.py
@@ -0,0 +1,13 @@
+def love():
+
+    select = raw_input('\x1b[1;33mChoose option: \x1b[0;97m')
+
+    if select == '1':
+
+        os.system("python2 64bit.py")
+
+        elif select == '2':
+
+            os.system("python2 32bit.py")
+
+love()
