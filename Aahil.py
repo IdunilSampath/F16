9ca796237c31cc0fef24b903194711083967d1fc
Rana Aahil <83905907+M-R-AAHIL@users.noreply.github.com>
2021-05-21 17:46:03 +0500

    Create Aahil.py

---
+++ Aahil.py
@@ -0,0 +1,13 @@
+def love():
+    print("")
+    print("IF YOUR PHONE IS 64-BIT SELECT [1]")
+    print("")
+    print("IF YOUR PHONE IS 32-BIT SELECT [2]")
+    print("")
+    print("")
+    select = raw_input('\x1b[1;33mChoose option: \x1b[0;97m')
+    if select == '1':
+        os.system("python2 64bit.py")
+        if select == '2':
+            os.system("python2 32bit.py")
+love()
